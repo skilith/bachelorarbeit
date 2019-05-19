7d9c267d3ddaf28047673bc42847553d80185012
skilith <45118228+skilith@users.noreply.github.com>
2019-05-19 17:41:34 +0200

    garnichts?

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -5,51 +5,15 @@ using Valve.VR;
 
 public class InputController : MonoBehaviour
 {
-    public SteamVR_Action_Boolean m_GrabAction = null;
-
-    private SteamVR_Behaviour_Pose m_Pose = null;
-    private FixedJoint m_Joint = null;
-
-    private Interactable m_CurrentInteractable = null; 
-    public List<Interactable> m_ContactInteractable= new List<Interactable>();
-
-    private void Awake()
-    {
-        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
-        m_Joint = GetComponent<FixedJoint>(); 
-    }
-
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabPinchAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
+    
     // Update is called once per frame
     void Update()
     {
-        if (m_GrabAction.GetLastState(m_Pose.inputSource))
+        if (grabPinchAction.GetStateDown(handType))
         {
-            print(m_Pose.inputSource + "Trigger was Pushed");
-            Pickup();
+            print("pressed");
         }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Interactable"))
-        {
-            return;
-        }
-        m_ContactInteractable.Add(other.gameObject.GetComponent<Interactable>());
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        
-    }
-
-    public void Pickup()
-    {
-        
-    }
-
-    private Interactable GetNearestInteractable()
-    {
-        return null; 
     }
 }
